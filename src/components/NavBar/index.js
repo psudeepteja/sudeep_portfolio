354ac91170d9bd2c32e8407e26b78429903535cc
Pandipati Teja <Pandipati.T@allfon.in>
2025-06-23 14:41:55 +0530

    UI fixes and added react silk lib for projects

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -11,30 +11,37 @@ export default function NavBar() {
         setIsOpen(!isOpen)
     }
 
+    const navLinks = [
+        { name: "About", link: "#about" },
+        { name: "Skills", link: "#skills" },
+        { name: "Projects", link: "#projects" },
+        { name: "Contact", link: "#contact" },
+    ]
+
     return (
         <>
-            <div className='md:hidden flex justify-end items-center' onClick={handleClick}>
+            <div className='md:hidden flex justify-end items-center' >
                 <Theme />
                 {!isOpen ? (
-                    <MaterialSymbolsMenu />
-                ):(
-              <div className="flex justify-end" onClick={handleClick}><GgClose /></div>
+                    <div onClick={handleClick}>
+                        <MaterialSymbolsMenu />
+                    </div>
+                ) : (
+                    <div className="flex justify-end" onClick={handleClick}><GgClose /></div>
                 )}
             </div>
             {isOpen && (
                 <div className='md:hidden flex justify-end flex-col absolute top-12 right-0 w-full my-2 bg-white dark:bg-gray-800 px-4 py-2 shadow-xl'>
-                    <a href="#about" >About</a>
-                    <a href="#skills" >Skills</a>
-                    <a href="#projects" >Projects</a>
-                    <a href="#contact" >Contact</a>
+                    {navLinks.map((item, idx) => (
+                        <a key={idx} href={item.link} onClick={()=>setIsOpen(false)}>{item.name}</a>
+                    ))}
                 </div>
             )}
             <div className='hidden md:flex md:justify-end items-center gap-4'>
                 <Theme />
-                <a href="#about" className='hidden md:flex'>About</a>
-                <a href="#skills" className='hidden md:flex'>Skills</a>
-                 <a href="#projects" className='hidden md:flex'>Projects</a>
-                <a href="#contact" className='hidden md:flex'>Contact</a>
+                {navLinks.map((item, idx) => (
+                    <a key={idx} href={item.link} className='hidden md:flex' >{item.name}</a>
+                ))}
             </div>
         </>
     )
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,42 +1,120 @@
+// import { skills } from '@/constant/skills'
+// import Image from 'next/image'
+
+// export default function Projects() {
+//     const webPages = [
+//         {title: "ShowTime App", url:"https://show-time-app.vercel.app/movies/nellore", img:"/ShowTime.png"},
+//         {title: "Russell And Bromley", url:"https://www.russellandbromley.co.uk", img:"/Russell_and_Bromley_UK.png"},
+//         {title: "Liverpool Suburbia", url:"https://www.suburbia.com.mx/tienda/home", img:"/Suburbia.jpg"},
+//         {title: "ECart App", url:"https://e-cart-app-beige.vercel.app", img:"/ECart1.png"},
+//         {title: "Recipie App", url:"https://vgts-task-green.vercel.app", img:"/Recipie.png"},
+//         // {title: "Bill Buddy App", url:"https://bill-buddy-app.vercel.app"},
+
+//     ]
+//     return (
+//         <div className="px-4 py-6 md:px-4 md:py-16">
+//             <div className='text-center pb-8'>
+//             <span className="bg-[#FF6464] text-[#fff] px-4 py-1 inline rounded-xl">Projects</span>
+//             </div>
+//              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 ">
+//         {webPages.map((item, index) => (
+//           <a
+//             key={index}
+//             href={item.url}
+//             target="_blank"
+//             rel="noopener noreferrer"
+//             className="block bg-white dark:bg-gray-950 rounded-xl shadow-lg inset-shadow-[#FF6464] hover:shadow-lg transition-transform transform hover:scale-105 overflow-hidden border dark:border-gray-900"
+//           >
+//             {/* <img
+//               src={item.img}
+//               alt={item.title}
+//               className="w-full h-24 object-cover"
+//             /> */}
+//             <div className="p-4 text-center">
+//               <h3 className="text-lg font-semibold">{item.title}</h3>
+//             </div>
+//           </a>
+//         ))}
+//       </div>
+
+//         </div>
+//     )
+// }
+"use client"
+
 import { skills } from '@/constant/skills'
 import Image from 'next/image'
+import React from "react";
+import Slider from "react-slick";
+import './styles.css'
+
 
 export default function Projects() {
-    const webPages = [
-        {title: "ShowTime App", url:"https://show-time-app.vercel.app/movies/nellore", img:"/ShowTime.png"},
-        {title: "Russell And Bromley", url:"https://www.russellandbromley.co.uk", img:"/Russell_and_Bromley_UK.png"},
-        {title: "Liverpool Suburbia", url:"https://www.suburbia.com.mx/tienda/home", img:"/Suburbia.jpg"},
-        {title: "ECart App", url:"https://e-cart-app-beige.vercel.app", img:"/ECart1.png"},
-        {title: "Recipie App", url:"https://vgts-task-green.vercel.app", img:"/Recipie.png"},
-        // {title: "Bill Buddy App", url:"https://bill-buddy-app.vercel.app"},
+  const webPages = [
+    { title: "ShowTime App", url: "https://show-time-app.vercel.app/movies/nellore", img: "/ShowTime.png" },
+    { title: "Russell And Bromley", url: "https://www.russellandbromley.co.uk", img: "/Russell_and_Bromley_UK.png" },
+    { title: "Liverpool Suburbia", url: "https://www.suburbia.com.mx/tienda/home", img: "/Suburbia.jpg" },
+    { title: "ECart App", url: "https://e-cart-app-beige.vercel.app", img: "/ECart1.png" },
+    { title: "Recipie App", url: "https://vgts-task-green.vercel.app", img: "/Recipie.png" },
+    // {title: "Bill Buddy App", url:"https://bill-buddy-app.vercel.app"},
 
+  ]
+
+  const settings = {
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 4000,
+    autoplaySpeed: 100,
+    // cssEase: "linear"
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
     ]
-    return (
-        <div className="px-4 py-6 md:px-4 md:py-16">
-            <div className='text-center pb-8'>
-            <span className="bg-[#FF6464] text-[#fff] px-4 py-1 inline rounded-xl">Projects</span>
-            </div>
-             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 ">
-        {webPages.map((item, index) => (
-          <a
-            key={index}
-            href={item.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="block bg-white dark:bg-gray-950 rounded-xl shadow-lg inset-shadow-[#FF6464] hover:shadow-lg transition-transform transform hover:scale-105 overflow-hidden border"
-          >
-            <img
+  };
+
+  return (
+    <div className="px-4 py-6 md:px-4 md:py-16">
+      <div className='text-center pb-8'>
+        <span className="bg-[#FF6464] text-[#fff] px-4 py-1 inline rounded-xl">Projects</span>
+      </div>
+      <div>
+        <p className="py-2">Here are some of the projects I’ve worked on, showcasing my skills in frontend development, and building responsive, user-centric web applications. Each project highlights a different aspect of my technical abilities, from integrating APIs to implementing scalable component-based architectures.</p>
+      </div>
+      <div className="slider-container">
+        <Slider {...settings}>
+          {webPages.map((item, index) => (
+            <a
+              key={index}
+              href={item.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block bg-white dark:bg-gray-950 rounded-xl shadow-lg inset-shadow-[#FF6464] hover:shadow-lg transition-transform transform hover:scale-105 overflow-hidden border dark:border-gray-900"
+            >
+              {/* <img
               src={item.img}
               alt={item.title}
               className="w-full h-24 object-cover"
-            />
-            <div className="p-4 text-center">
-              <h3 className="text-lg font-semibold">{item.title}</h3>
-            </div>
-          </a>
-        ))}
+            /> */}
+              <div className="p-4 text-center">
+                <h3 className="text-lg font-semibold">{item.title}</h3>
+              </div>
+            </a>
+          ))}
+        </Slider>
       </div>
- 
-        </div>
-    )
+    </div>
+  )
 }
+
