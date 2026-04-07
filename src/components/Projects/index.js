a354237677d66932fd83d77005c46f785184710e
Pandipati  Teja <sudeepteja.p@excelacom.in>
2026-04-07 18:43:09 +0530

    new changes

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -7,14 +7,21 @@ import { ShowTimeIcon } from "@/icons/ShowTime";
 import { RussellBromleyIcon } from "@/icons/RussellBromley";
 import { LiverpoolSuburbiaIcon } from "@/icons/LiverpoolSuburbia";
 import { ECartIcon } from "@/icons/ECart";
-import { RecipeIcon } from "@/icons/Recipe";
+// import { RecipeIcon } from "@/icons/Recipe";
 
 export default function Projects() {
   const webPages = [
     { title: "ShowTime App", url: "https://show-time-app.vercel.app/movies/nellore", icon: <ShowTimeIcon /> },
     { title: "Russell And Bromley", url: "https://www.russellandbromley.co.uk", icon: <RussellBromleyIcon /> },
     { title: "Liverpool Suburbia", url: "https://www.suburbia.com.mx/tienda/home", icon: <LiverpoolSuburbiaIcon /> },
-    { title: "ECart App", url: "https://e-cart-app-beige.vercel.app", icon: <ECartIcon /> },
+    {
+      title: "", url: "https://nexcart-app.vercel.app", icon: (
+        <div className="flex h-[96px] items-center">
+          <span className="font-bold text-2xl text-[#0FA3B1]">Nex</span>
+          <span className="font-bold text-2xl text-[#7B2CBF]">Cart</span>
+        </div>
+      )
+    },
     //{ title: "Recipie App", url: "https://vgts-task-green.vercel.app", icon: <RecipeIcon /> },
     // {title: "Bill Buddy App", url:"https://bill-buddy-app.vercel.app"},
   ]
@@ -61,9 +68,9 @@ export default function Projects() {
               rel="noopener noreferrer"
               className={`block rounded-xl shadow-md inset-shadow-[#FF6464] hover:shadow-lg transition-transform transform hover:scale-105 overflow-hidden border dark:border-gray-900 : ""}`}
             >
-              <div className="p-4 flex flex-col items-center justify-center">
-                {item.icon}
-                <h3 className="font-bold">{item.title}</h3>
+              <div className="p-4 flex flex-col items-center justify-center min-h-[96px]">
+                {item?.icon}
+                <h3 className="font-bold">{item?.title}</h3>
               </div>
             </a>
           ))}
